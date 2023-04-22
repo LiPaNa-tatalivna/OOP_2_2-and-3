a49e7b115fd58acb644df3f76b8b999af4fadef4
Lipana) <liza-fox02@mail.ru>
2023-04-22 20:19:11 +0300

    Initial Commit

---
+++ ООП_23/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ООП_23
+//
+//  Created by Liza Stepanova on 22.04.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
